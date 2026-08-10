0ce75a5970864e1e98205a0fbea5a26f338c2889
uk1241 <unnivr011@gmail.com>
2026-08-10 11:44:55 +0530

    server requirement cofiguration

--- textile_tracker/settings.py
+++ textile_tracker/settings.py
@@ -25,7 +25,13 @@ SECRET_KEY = "django-insecure-_@=6adztx&h$pv&+&uxs06mqim%vttc*i7%+(n6&(hu_-p+jf8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+import os
+
+ALLOWED_HOSTS = [
+    "wearon-os.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
